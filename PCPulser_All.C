280f327664bd1f10457f6e09f73acdd5643babc4
Jon Lighthall <jonlighthall@users.noreply.github.com>
2016-09-08 17:40:39 +0000

    Summary: organized run-specific settings, removed whitespace

--- PCPulser_All.C
+++ PCPulser_All.C
@@ -1,43 +1,52 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {
-  //run250//run251 //
+  //run250//
   //const Int_t npeaks = 6;
   //Float_t Volts[npeaks] = {0.5, 1.0, 3.0, 5.0, 7.0, 9.0};
+  //TFile *f1 = new TFile("/data0/nabin/ANASEN_KTM/New/evt2root/run250_NSCL11_Pulser.root");  
+  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/New/evt2root/run250_NSCL11_Pulser.root");  
 
-   //run262//
+  //run262//
   //const Int_t npeaks = 5;
   //Float_t Volts[npeaks] = {0.05, 0.1, 0.15, 0.2, 0.25};
-
   //const Int_t npeaks = 4;
   //Float_t Volts[npeaks] = {0.1, 0.15, 0.2, 0.25};
+  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/262.root");  
 
-  const Int_t npeaks = 5;
+  //run640//
+  //const Int_t npeaks = 5;
   //Float_t Volts[npeaks] = {0.1, 0.2, 0.3, 0.4, 0.5};
-  Float_t Volts[npeaks] = {0.06, 0.12, 0.3, 0.6, 0.8};
+  //Float_t Volts[npeaks] = {0.06, 0.12, 0.3, 0.6, 0.8};
   //Float_t Volts2[4] = {0.2, 0.3, 0.4, 0.5};
-  Float_t Volts2[4] = {0.12, 0.3, 0.6, 0.8};
+  //Float_t Volts2[4] = {0.12, 0.3, 0.6, 0.8};
+  //TFile *f1 = new TFile("/home/manasta/Desktop/parker_codes/evt2root_files/run640.root");  
+  
+  //run942 18Ne data
+  //const Int_t npeaks = 6;
+  //Float_t Volts[npeaks] = {0.006, 0.012, 0.03, 0.06, 0.08,0.12};
+  //Float_t Volts2[4] = {0.03, 0.06, 0.08, 0.12};
+  //TFile *f1 = new TFile("/home/manasta/Desktop/parker_codes/evt2root_files/run942.root"); 
+  //TFile *f1 = new TFile("/data0/manasta/evt2root_files/run942.root"); 
 
+  //run1036 24Mg data
+  const Int_t npeaks = 6;
+  Float_t Volts[npeaks] = {0.003, 0.006, 0.01, 0.03, 0.06, 0.1};
+  Float_t Volts2[4] = {0.01, 0.03, 0.06, 0.1};
+  TFile *f1 = new TFile("/data1/lighthall/root/run1036.root"); 
+  
   TCanvas *c1 = new TCanvas();
   c1->Divide(1,2);
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN_KTM/New/evt2root/run250_NSCL11_Pulser.root");  
-
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/New/evt2root/run250_NSCL11_Pulser.root");  
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/262.root");  
-  TFile *f1 = new TFile("/home/manasta/Desktop/parker_codes/evt2root_files/run640.root");  
-
+  
   TH1I *h1 = new TH1I("h1","h1",512,0,6000);
   TTree *DataTree = (TTree*)f1->Get("DataTree");
-  
-  
+    
   ofstream outfile;
   outfile.open("PCpulserCal2016May11.dat");
-	
+  
   for (Int_t id=2; id<4; id++) {
     for (Int_t chan=0; chan<32; chan++) {
-      
-    
+
       if(id==3 && chan>15) continue;
-    
 
       c1->cd(1);
       DataTree->Draw("ADC.Data>>h1",Form("ADC.ID==%d && ADC.ChNum==%d && ADC.Data>200 && ADC.Data<5000",id,chan));
@@ -45,7 +54,6 @@
       
       TSpectrum *s = new TSpectrum(npeaks+1);
       h1->SetTitle(Form("ADC %i Chan %i",id,chan));
-      
       
       Int_t nfound = s->Search(h1,9," ",0.09);
       c1->Update();
