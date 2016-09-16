d272319b731d1dd8dd40be5672dbfa7c698902a0
Jon Lighthall <jlighthall@fsu.edu>
2016-09-16 16:54:18 +0000

    Summary: minor formatting changes to prepare for merging with other two files

--- PCCal/PulserCal/PCPulser_All.C
+++ PCCal/PulserCal/PCPulser_All.C
@@ -3,43 +3,38 @@
   //run250//run251 //
   //const Int_t npeaks = 6;
   //Float_t Volts[npeaks] = {0.5, 1.0, 3.0, 5.0, 7.0, 9.0};
+  //TFile *f1 = new TFile("/data0/nabin/ANASEN_KTM/New/evt2root/run250_NSCL11_Pulser.root");  
+  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/New/evt2root/run250_NSCL11_Pulser.root");
 
-   //run262//
+  //run262//
   //const Int_t npeaks = 5;
   //Float_t Volts[npeaks] = {0.05, 0.1, 0.15, 0.2, 0.25};
-
   //const Int_t npeaks = 4;
   //Float_t Volts[npeaks] = {0.1, 0.15, 0.2, 0.25};
+  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/262.root");  
 
   const Int_t npeaks = 5;
   Float_t Volts[npeaks] = {0.1, 0.2, 0.3, 0.4, 0.5};
-
   Float_t Volts2[4] = {0.2, 0.3, 0.4, 0.5};
+  TFile *f1 = new TFile("/home2/parker/ANASEN/LSU/evt2root_root/run354.root");  
 
   TCanvas *c1 = new TCanvas();
   c1->Divide(1,2);
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN_KTM/New/evt2root/run250_NSCL11_Pulser.root");  
-
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/New/evt2root/run250_NSCL11_Pulser.root");  
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/262.root");  
-  TFile *f1 = new TFile("/home2/parker/ANASEN/LSU/evt2root_root/run354.root");  
-
+  
   TH1I *h1 = new TH1I("h1","h1",256,0,4095);
   TTree *DataTree = (TTree*)f1->Get("DataTree");
-  
   
   ofstream outfile;
   outfile.open("PCpulserCal2016Apr20.dat");
 	
   for (Int_t id=2; id<4; id++) {
     for (Int_t chan=0; chan<32; chan++) {
-      
     
       if(id==3 && chan>15) continue;
-    
 
       c1->cd(1);
       DataTree->Draw("ADC.Data>>h1",Form("ADC.ID==%d && ADC.ChNum==%d && ADC.Data>500 && ADC.Data<3900",id,chan));
+    //DataTree->Draw("ADC.Data>>h1",Form("ADC.ID==%d && ADC.ChNum==%d && ADC.Data>180 && ADC.Data<4900",id,chan));
       if(h1->GetEntries()==0) continue;
       
       TSpectrum *s = new TSpectrum(npeaks+1);
@@ -47,6 +42,7 @@
       
       
       Int_t nfound = s->Search(h1,9," ",0.09);
+    //Int_t nfound = s->Search(h1,3," ",0.05); //Search(histo,sigma,option,threshold)
       c1->Update();
       //c1->WaitPrimitive();
       
@@ -65,10 +61,10 @@
 	    
       for (Int_t l=0; l<nfound; l++) {
         if(nfound>npeaks)
-        {
-          cout << "Warning: Wire " << id << "(" << chan << ")" << "has too many peaks" << endl;
-          break;
-        }
+	  {
+	    cout << "Warning: Wire " << id << "(" << chan << ")" << "has too many peaks" << endl;
+	    break;
+	  }
         //outfile << id << "\t" << chan << "\t" << Volts[l] << "\t" << xpeaks[l] << endl;
       }
       c1->cd(2);
@@ -90,4 +86,4 @@
     }
   }
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
