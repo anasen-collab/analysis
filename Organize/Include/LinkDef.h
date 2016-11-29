e5bf34ddcc5a280f4c87e499fcec6a047ae22dba
Jon Lighthall <jonlighthall@users.noreply.github.com>
2016-11-29 18:03:20 +0000

    adding organize file folder

---
+++ Organize/Include/LinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+ 
+//#pragma link C++ defined_in "/home2/parker/ANASEN/LSU/Include/organizetree.h";
+#pragma link C++ defined_in "/home/manasta/Desktop/parker_codes/Include/organizetree.h";
+
+#endif
