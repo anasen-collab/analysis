f9c3dd5a828c1247cd0ff73976de19a1b800c400
Jon Lighthall <jonlighthall@users.noreply.github.com>
2016-11-29 17:54:50 +0000

    adding updated relative calibration files

---
+++ SiCal/SiRelativeCal/SX3s/SiRelativeGains_Clickable.C
@@ -0,0 +1,137 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////This program fixes the relative gains for the up and down on the SX3
+////How to use: Create Histograms in the Main.C:
+////-----Plot down vs up energy for each front channel of each detector
+////-----Code reads in histogram of name down_vs_up%i_front%i--e.g. down_vs_up4_front0 (det4, channel0)
+////-----It can loop over any range of detectors you want, but if the histogram does not exist, the code will crash and your work will not be saved
+////
+////-----Craching fixed by putting an if-statement when histo is NULL continue //M.Anastasiou
+////
+////The program reads in an X3RelativeGains.dat file and outputs a new file with updated coefficients
+////Before running, make sure that the root file you are reading in has the right histograms and has been created using the X3RelativeGains.dat file that you are inputting into this code
+////
+////This file changes the relative gains on the down relative to the up
+////
+////root -l SiRelativeGains_Clickable.C++
+////in canvas: View->Toolbar->GraphicalCut (pair of scissors on right)
+////the plot should look like a straight line. click along the straight line. when you are done, double click in canvas and a best fit line will appear. The best fit line should follow the data very well, if not you are doing something wrong.
+////Because the back gains are not set properly, the line may appear segmented. If so, choose your favorite segment and get a best fit line for that. Do not click in each segment as that will throw your best fit line off. All of the segments for a detector should have the same slope.
+////
+////After the best fit line appears, double click to move onto the next channel
+////
+////
+////Once you have completed this program, rerun OrganizeIC.cpp with this new X3RelativeGains_Step1.dat
+////You will input this new root file with this new relative gains file into step 2.
+
+//// Edited by : John Parker , 2016Jan24
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <TMath.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TGraph.h>
+#include <TF1.h>
+#include <TSpectrum.h>
+#include <fstream>
+#include <exception>
+#include <TCutG.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void SiRelativeGains_Clickable(void){
+
+  using namespace std;
+  Double_t x[10];
+  Double_t y[10];
+  TFile *f1 = new TFile("/data0/manasta/OrganizeRaw_files/run930_931_nospacer_X3slope1_divideback.root"); // root file name created with OrganizeIC.cpp
+
+  ofstream outfile;
+  outfile.open("X3RelativeGains09192016_Step1_det27.dat"); //output file name
+
+  //if you are just starting the calibration you can use an input file where ALL SLOPES ARE ONE(1) apart from the MASKED CHANNELS which are ZERO(0)
+
+  ifstream infile;
+  infile.open("X3RelativeGains09192016_Step1_det26.dat"); //input file name
+  Int_t det=0,ch=0;
+  Double_t dummy_slope = 0;
+  Double_t slope[24][12];
+  if (infile.is_open()){
+    while (!infile.eof()){
+      infile >> det >> ch >> dummy_slope;
+      slope[det-4][ch] = dummy_slope;
+    }
+  }else{
+    cout << "Infile not opened\n";
+    exit(EXIT_FAILURE);
+  }
+  infile.close();
+
+  TCanvas *can = new TCanvas("can","can",800,600);
+  TCutG *cut;
+
+  Int_t bad_det[288];
+  Int_t bad_front[288];
+  Int_t count_bad = 0;
+
+  //You can do the calibration detector by detector looping one detector at a time or loop from DetNum=4 to 27.
+  //if you do so change the loop below.
+
+  //histo "down_vs_up_divideBack%i_front_%i" is a new extra histo that was created in the OrganizeIC.cpp for data that are normalized with the BackEnergy
+  //if your data is not normalized use histo "down_vs_up%i_front_%i" for this code. 
+
+  for (Int_t DetNum=27; DetNum<28; DetNum++){
+    for (Int_t FrontChNum=0; FrontChNum<4; FrontChNum++){
+     
+      TH2F *hist = NULL;
+      hist = (TH2F*)f1->Get(Form("down_vs_up_divideBack%i_front%i",DetNum,FrontChNum)); 
+      if (hist==NULL){
+	cout << "Histo does not exist\n";
+	bad_det[count_bad] = DetNum;
+	bad_front[count_bad] = FrontChNum;
+	count_bad++;
+	continue;
+      }  
+      
+      hist->Draw("colz");
+	
+      cut = (TCutG*)can->WaitPrimitive("CUTG");
+	
+      for(int n=0;n<cut->GetN()-2;n++){
+	cut->GetPoint(n,x[n],y[n]);
+	cout << x[n] << "\t" << y[n] << endl;
+      }
+	
+      TGraph *graph = new TGraph(cut->GetN()-2,x,y);
+      hist->Draw("colz");
+      graph->Draw("*same");
+	
+      TF1 *fun = new TF1("fun","[0] + [1]*x",0,1);
+      graph->Fit("fun");
+	
+      can->Update();
+      can->WaitPrimitive();
+
+      Double_t average_slope = fun->GetParameter(1);
+      slope[DetNum-4][FrontChNum+8] = -slope[DetNum-4][FrontChNum+8]/average_slope;
+    }  
+  }
+
+  for (Int_t i=0; i<24; i++){
+    for (Int_t j=0; j<12; j++){
+      outfile << i+4 << "\t" << j << "\t" << slope[i][j] << endl;
+    }
+  } 
+  outfile.close();
+  cout << "List of bad detectors:\n";
+  for (int i=0; i<count_bad; i++){
+    cout << bad_det[i] << "  " << bad_front[i] << endl;
+  }
+  delete can;
+}
+
+
+
+
---
+++ SiCal/SiRelativeCal/SX3s/SiRelativeGains_Step3.C
@@ -0,0 +1,170 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// Relative calibration of Si gains
+////
+////This step fixes the relative gains of the 3 remaining back channels to that of a designated front channel
+////It works in the same was as step 2
+////The back gain is equal to the inverse of the slope of the best fit line.
+////
+////First, histograms should be created for events in which one front strip (up and down) and one back strip fired. Using more complicated multiplicities here will confuse things (if only the up fired, then front != back, which defeats the initial assumption that front == back).
+////Whichever gains file was used in the program that creates the histos should be the input file to this program.
+////Input histogram file and relative gains file
+////Things to watch out for:
+////In the MyFit() funtion, the binning is assumed. This could cause runtime errors (probably won't crash though) if your binning is different than what the program assumes.
+////
+////root -l SiRelativeGains_Step3.C++
+////
+//// Edited by : John Parker , 2016Jan22
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <TMath.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TGraph.h>
+#include <TF1.h>
+#include <TSpectrum.h>
+#include <fstream>
+#include <exception>
+#include <TCutG.h>
+#include <TVector.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Double_t MyFit(TH2F* hist, TCanvas *can){
+  hist->Draw("colz");
+
+  Double_t maxbinNumberX = hist->GetXaxis()->GetXmax();
+  Double_t maxbinNumberY = hist->GetYaxis()->GetXmax();
+  Double_t maxbinX = (maxbinNumberX/hist->GetNbinsX());
+  Double_t maxbinY = (maxbinNumberY/hist->GetNbinsY());
+
+  // Double_t x1[5] = { 2, 12, 9, 0.4, 2 };
+  //Double_t y1[5] = { 0.8, 9.8, 11, 1.1, 0.8 };
+  //TCutG *cut = new TCutG("cut",5,x1,y1);
+
+  Int_t counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      //if ( !cut->IsInside((Double_t)i*0.1,j*0.1) ){
+      //	continue;
+      //}
+      for (int k=0; k<hist->GetBinContent(i,j); k++){
+	counter++;
+      }
+    }
+  }
+
+  Double_t *x = new Double_t[counter];
+  Double_t *y = new Double_t[counter];
+
+  counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      //if ( !cut->IsInside((Double_t)i*0.1,j*0.1) ){
+      //	continue;
+      //}
+      for (int k=0; k<hist->GetBinContent(i,j); k++){
+	x[counter] = i*maxbinX;
+	y[counter] = j*maxbinY;
+	counter++;
+      }
+    }
+  }
+
+  TGraph *graph = new TGraph(counter,x,y);
+  graph->Draw("*same");
+
+  TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,10000);
+  graph->Fit("fun2");
+  can->Update();
+  can->WaitPrimitive();
+
+  Double_t gain = fun2->GetParameter(0);
+  delete x;
+  delete y;
+  delete graph;
+  delete fun2;
+
+  return gain;
+}
+
+void SiRelativeGains_Step3(void)
+{
+  using namespace std;
+
+  TFile *f1 = new TFile("/data0/manasta/OrganizeRaw_files/run930_931_nospacer_X3step2_divideback.root");
+  if ( !f1->IsOpen() ){
+    cout << "Error: Root File Does Not Exist\n";
+    exit(EXIT_FAILURE);
+  }
+  TCanvas *can = new TCanvas("can","can",800,600);
+  
+  ofstream outfile;
+
+  outfile.open("X3RelativeGains09202016_Step3.dat");
+  //outfile.open("test_det26.dat");
+
+  ifstream infile;
+  infile.open("X3RelativeGains09202016_Step2_all.dat");
+  Int_t det=0,ch=0;
+  Double_t slope[24][12];
+  Double_t dummy;
+  if (infile.is_open()){
+    while (!infile.eof()){
+      infile >> det >> ch >> dummy;
+      slope[det-4][ch] = dummy;
+    }
+  }else{
+    cout << "Infile not opened\n";
+    exit(EXIT_FAILURE);
+  }
+  infile.close();
+
+  Int_t bad_det[288];
+  Int_t bad_front[288];
+  Int_t bad_back[288];
+  Int_t count_bad = 0;
+
+  Double_t gain = 0;
+
+  for (Int_t DetNum=4; DetNum<28; DetNum++){
+    
+    for (Int_t BackChNum=0; BackChNum<4; BackChNum++){
+      if ( DetNum==8 ){
+      	BackChNum = 3;
+      }
+      
+     Int_t FrontChNum = 1;
+      
+
+      TH2F *hist = NULL;
+      hist = (TH2F*)f1->Get(Form("back_vs_front%i_back%i",DetNum,BackChNum));
+      if (hist==NULL){
+	cout << "Histo does not exist\n";
+	bad_det[count_bad] = DetNum;
+	bad_front[count_bad] = FrontChNum;
+	bad_back[count_bad] = BackChNum;
+	count_bad++;
+	continue;
+      }
+
+      gain = MyFit(hist,can);
+      slope[DetNum-4][BackChNum] = slope[DetNum-4][BackChNum]/gain;
+    }
+    for (int i=0; i<12; i++){
+      outfile << DetNum << "\t" << i << "\t" << slope[DetNum-4][i] << endl;
+    }
+  }
+  outfile.close();
+  cout << "List of bad detectors:\n";
+  for (int i=0; i<count_bad; i++){
+    cout << bad_det[i] << "  " << bad_front[i] << "  " << bad_back[i] << endl;
+  }
+
+  delete can;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		 
+
+	    
+	     
+    
