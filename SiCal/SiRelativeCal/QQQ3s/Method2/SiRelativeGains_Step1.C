056972c0bc89fadeb0873b3c0c63d1e2d338e976
Jon Lighthall <jlighthall@fsu.edu>
2017-01-03 14:59:51 +0000

    removing extra folder

--- SiCal/SiRelativeCal/QQQ3s/Method2/SiRelativeGains_Step1.C
+++ SiCal/SiRelativeCal/QQQ3s/Method2/SiRelativeGains_Step1.C
@@ -1,9 +1,13 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////Relative calibration of Si gains for QQQ
-////Essentially the same progam as that for the SX3
-////root -l SiRelativeGains_Step1.C+
-
-
+//// Relative calibration of Si gains--Method2
+////This method works very similar to method 1, except that instead of a predefined cut, the user must draw their own graphical cut
+////To do this, in the canvas: View->Toolbar and click on the scissors on the top right hand corner
+////Then, select the region around your data by clicking. Double click to close the cut.
+////A best fit line should appear through your data
+////
+////Things to watch out for:
+////In the MyFit() funtion, the binning is assumed. This could cause runtime errors (probably won't crash though) if your binning is different than what the program assumes.
+////
 //// Edited by : John Parker , 2016Jan22
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <TMath.h>
@@ -24,37 +28,30 @@
 Double_t MyFit(TH2F* hist, TCanvas *can){
   hist->Draw("colz");
 
-   
-  Double_t x1[12] = {1450, 630, 3150, 6340, 9200, 10540, 13200, 13600, 11670, 7550, 2400, 1450};
-  Double_t y1[12] = {800, 2250, 4900, 8050, 10380, 11520, 13800, 12600, 8700, 5400, 1100, 800};
-  TCutG *cut = new TCutG("cut",12,x1,y1);
-  //cut = (TCutG*)can->WaitPrimitive("CUTG");
-  
-  Double_t maxbinNumberX = hist->GetXaxis()->GetXmax();
-  Double_t maxbinNumberY = hist->GetYaxis()->GetXmax();
-  Double_t maxbinX = (maxbinNumberX/hist->GetNbinsX());
-  Double_t maxbinY = (maxbinNumberY/hist->GetNbinsY());
+  vector<double> x1;
+  vector<double> y1;
 
-  /*cout << "maxbinNumberX " << maxbinNumberX << endl;
-  cout << "maxbinNumberY " << maxbinNumberY << endl;
-  cout << "binX " << hist->GetNbinsX() << endl;
-  cout << "maxbinsX" << maxbinX << endl;
-  cout << "binY " << hist->GetNbinsY() << endl;
-  cout << "maxbinsY" << maxbinY << endl;*/
+  TCutG *cut;
+  cut = (TCutG*)can->WaitPrimitive("CUTG");
+  x1.resize(cut->GetN());
+  y1.resize(cut->GetN());
 
+  for(int n=0;n<cut->GetN();n++){
+    cut->GetPoint(n,x1[n],y1[n]);
+    cout << x1[n] << "\t" << y1[n] << endl;
+  }
 
   Int_t counter = 0;
   for (int i=1; i<hist->GetNbinsX(); i++){
     for (int j=1; j<hist->GetNbinsY(); j++){
-      if ( !cut->IsInside((Double_t)i*maxbinX,j*maxbinY)){
-      continue;
-       }
+      if ( !cut->IsInside((Double_t)i*20,j*20) ){
+	continue;
+      }
       for (int k=0; k<hist->GetBinContent(i,j); k++){
 	counter++;
       }
     }
   }
-
 
   Double_t *x = new Double_t[counter];
   Double_t *y = new Double_t[counter];
@@ -62,12 +59,12 @@ Double_t MyFit(TH2F* hist, TCanvas *can){
   counter = 0;
   for (int i=1; i<hist->GetNbinsX(); i++){
     for (int j=1; j<hist->GetNbinsY(); j++){
-      if ( !cut->IsInside((Double_t)i*maxbinX,j*maxbinY) ){
-      continue;
+      if ( !cut->IsInside((Double_t)i*20,j*20) ){
+	continue;
       }
       for (int k=0; k<hist->GetBinContent(i,j); k++){
-	x[counter] = i*maxbinX; 
-	y[counter] = j*maxbinY; 
+	x[counter] = i*20;
+	y[counter] = j*20;
 	counter++;
       }
     }
@@ -76,7 +73,7 @@ Double_t MyFit(TH2F* hist, TCanvas *can){
   TGraph *graph = new TGraph(counter,x,y);
   graph->Draw("*same");
 
-  TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
+  TF1 *fun2 = new TF1("fun2","[0]*x",0,10000);
   graph->Fit("fun2");
   can->Update();
   can->WaitPrimitive();
@@ -87,6 +84,10 @@ Double_t MyFit(TH2F* hist, TCanvas *can){
   delete graph;
   delete fun2;
 
+  if (gain<0.1){
+    gain = 1;
+  }
+
   return gain;
 }
 
@@ -94,8 +95,7 @@ void SiRelativeGains_Step1(void)
 {
   using namespace std;
 
-  //TFile *f1 = new TFile("/data0/manasta/OrganizeRaw_files/run924_16O_sp7_slope1.root");
-  TFile *f1 = new TFile("/home/lighthall/repository/analysis/ANASEN/anasen_analysis_software/output.root"); 
+  TFile *f1 = new TFile("run235_245out_Step3_012816.root");//front
   if ( !f1->IsOpen() ){
     cout << "Error: Root File Does Not Exist\n";
     exit(EXIT_FAILURE);
@@ -103,17 +103,18 @@ void SiRelativeGains_Step1(void)
   TCanvas *can = new TCanvas("can","can",800,600);
 
   ofstream outfile;
-  outfile.open("QQQRelativeGains09182016_Step1.dat");
+
+  outfile.open("X3RelativeGains012816_Step4.dat");
 
   ifstream infile;
-  infile.open("QQQRelativeGains09122016_Slope1.dat");
+  infile.open("X3RelativeGains012816_Step3.dat");
   Int_t det=0,ch=0;
-  Double_t slope[4][32];
+  Double_t slope[24][12];
   Double_t dummy;
   if (infile.is_open()){
     while (!infile.eof()){
       infile >> det >> ch >> dummy;
-      slope[det][ch] = dummy;
+      slope[det-4][ch] = dummy;
     }
   }else{
     cout << "Infile not opened\n";
@@ -121,21 +122,24 @@ void SiRelativeGains_Step1(void)
   }
   infile.close();
 
-  Int_t bad_det[128];    
-  Int_t bad_front[128];
-  Int_t bad_back[128];
+  Int_t bad_det[288];
+  Int_t bad_front[288];
+  Int_t bad_back[288];
   Int_t count_bad = 0;
 
-  for (Int_t DetNum=0; DetNum<4; DetNum++){
-    for (Int_t FrontChNum=0; FrontChNum<16; FrontChNum++){
+  for (Int_t DetNum=27; DetNum<28; DetNum++){
+    for (Int_t i=0; i<4; i++){
+      outfile << DetNum << "\t" << i << "\t" << slope[DetNum-4][i] << endl;
+    }
+    for (Int_t FrontChNum=0; FrontChNum<4; FrontChNum++){
       Int_t BackChNum = 0;
-      
-      // if(DetNum==1 && (FrontChNum==4 || FrontChNum==14)){continue;}
-      //	 if(DetNum==2 && FrontChNum==11){continue;}
+      if ( DetNum==11 ){
+	BackChNum = 1;
+      }
 
       TH2F *hist = NULL;
       hist = (TH2F*)f1->Get(Form("back_vs_front%i_%i_%i",DetNum,FrontChNum,BackChNum));
-      if (hist==NULL){
+     if (hist==NULL){
 	cout << "Histo does not exist\n";
 	bad_det[count_bad] = DetNum;
 	bad_front[count_bad] = FrontChNum;
@@ -144,20 +148,22 @@ void SiRelativeGains_Step1(void)
 	continue;
       }
 
+
       Double_t gain = MyFit(hist,can);
-      slope[DetNum][FrontChNum+16] = slope[DetNum][FrontChNum+16]*gain;
+      slope[DetNum-4][FrontChNum+4] = slope[DetNum-4][FrontChNum+4]*gain;
+      slope[DetNum-4][FrontChNum+8] = slope[DetNum-4][FrontChNum+8]*gain;
+
     }
-    for (Int_t i=0; i<32; i++){
-      outfile << DetNum << "\t" << i << "\t" << slope[DetNum][i] << endl;
+    for (Int_t i=4; i<12; i++){
+      outfile << DetNum << "\t" << i << "\t" << slope[DetNum-4][i] << endl;
     }
   }
   outfile.close();
-  
-  cout << "List of bad detectors:\n";
+  cout << "List of bad detectos:\n";
   for (int i=0; i<count_bad; i++){
     cout << bad_det[i] << "  " << bad_front[i] << "  " << bad_back[i] << endl;
   }
-  
+
   delete can;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		 
