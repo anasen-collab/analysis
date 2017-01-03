b119d4915558440b8c5cbbf84e2475a05f5384b9
Jon Lighthall <jlighthall@fsu.edu>
2017-01-03 18:59:16 +0000

    Summary: drawing cut, fitting with ROB, missing hist name output, whitespace adjustments

--- SiCal/RelativeCal/QQQ/SiRelativeGains_Step1.C
+++ SiCal/RelativeCal/QQQ/SiRelativeGains_Step1.C
@@ -21,40 +21,43 @@
 #include <TVector.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Double_t MyFit(TH2F* hist, TCanvas *can){
+Double_t MyFit(TH2F* hist, TCanvas *can) {
   hist->Draw("colz");
-
    
   Double_t x1[12] = {1450, 630, 3150, 6340, 9200, 10540, 13200, 13600, 11670, 7550, 2400, 1450};
   Double_t y1[12] = {800, 2250, 4900, 8050, 10380, 11520, 13800, 12600, 8700, 5400, 1100, 800};
   TCutG *cut = new TCutG("cut",12,x1,y1);
   //cut = (TCutG*)can->WaitPrimitive("CUTG");
+  cut->Draw("same");
   
   Double_t maxbinNumberX = hist->GetXaxis()->GetXmax();
   Double_t maxbinNumberY = hist->GetYaxis()->GetXmax();
   Double_t maxbinX = (maxbinNumberX/hist->GetNbinsX());
   Double_t maxbinY = (maxbinNumberY/hist->GetNbinsY());
 
-  /*cout << "maxbinNumberX " << maxbinNumberX << endl;
-  cout << "maxbinNumberY " << maxbinNumberY << endl;
-  cout << "binX " << hist->GetNbinsX() << endl;
-  cout << "maxbinsX" << maxbinX << endl;
-  cout << "binY " << hist->GetNbinsY() << endl;
-  cout << "maxbinsY" << maxbinY << endl;*/
-
-
+  if(0) {
+    printf("axis range:\n");
+    cout << " maxbinNumberX " << maxbinNumberX << endl;
+    cout << " maxbinNumberY " << maxbinNumberY << endl;
+    printf("number of bins:\n");
+    cout << " binX " << hist->GetNbinsX() << endl;
+    cout << " binY " << hist->GetNbinsY() << endl;
+    printf("bin width:\n");
+    cout << " maxbinsX" << maxbinX << endl;
+    cout << " maxbinsY" << maxbinY << endl;
+  }
+  
   Int_t counter = 0;
-  for (int i=1; i<hist->GetNbinsX(); i++){
-    for (int j=1; j<hist->GetNbinsY(); j++){
-      if ( !cut->IsInside((Double_t)i*maxbinX,j*maxbinY)){
+  for (int i=1; i<hist->GetNbinsX(); i++) {
+    for (int j=1; j<hist->GetNbinsY(); j++) {
+      if ( !cut->IsInside((Double_t)i*maxbinX,j*maxbinY)) {
       continue;
        }
-      for (int k=0; k<hist->GetBinContent(i,j); k++){
+      for (int k=0; k<hist->GetBinContent(i,j); k++) {
 	counter++;
       }
     }
   }
-
 
   Double_t *x = new Double_t[counter];
   Double_t *y = new Double_t[counter];
@@ -77,9 +80,9 @@ Double_t MyFit(TH2F* hist, TCanvas *can){
   graph->Draw("*same");
 
   TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
-  graph->Fit("fun2");
+  graph->Fit("fun2","qROB");
   can->Update();
-  can->WaitPrimitive();
+  //can->WaitPrimitive();
 
   Double_t gain = fun2->GetParameter(0);
   delete x;
@@ -95,7 +98,8 @@ void SiRelativeGains_Step1(void)
   using namespace std;
 
   //TFile *f1 = new TFile("/data0/manasta/OrganizeRaw_files/run924_16O_sp7_slope1.root");
-  TFile *f1 = new TFile("/home/lighthall/repository/analysis/ANASEN/anasen_analysis_software/output.root"); 
+  //TFile *f1 = new TFile("/home/lighthall/repository/analysis/ANASEN/anasen_analysis_software/output.root");
+  TFile *f1 = new TFile("/home/lighthall/repository/analysis/ANASEN/root/run1209m.root"); 
   if ( !f1->IsOpen() ){
     cout << "Error: Root File Does Not Exist\n";
     exit(EXIT_FAILURE);
@@ -103,7 +107,7 @@ void SiRelativeGains_Step1(void)
   TCanvas *can = new TCanvas("can","can",800,600);
 
   ofstream outfile;
-  outfile.open("QQQRelativeGains09182016_Step1.dat");
+  outfile.open("QQQRelativeGains_Step1.dat");
 
   ifstream infile;
   infile.open("QQQRelativeGains09122016_Slope1.dat");
@@ -121,7 +125,7 @@ void SiRelativeGains_Step1(void)
   }
   infile.close();
 
-  Int_t bad_det[128];    
+  Int_t bad_det[128];
   Int_t bad_front[128];
   Int_t bad_back[128];
   Int_t count_bad = 0;
@@ -134,9 +138,10 @@ void SiRelativeGains_Step1(void)
       //	 if(DetNum==2 && FrontChNum==11){continue;}
 
       TH2F *hist = NULL;
-      hist = (TH2F*)f1->Get(Form("back_vs_front%i_%i_%i",DetNum,FrontChNum,BackChNum));
+      TString hname=Form("Q3_back_vs_front%i_%i_%i",DetNum,FrontChNum,BackChNum);
+      hist = (TH2F*)f1->Get(hname.Data());
       if (hist==NULL){
-	cout << "Histo does not exist\n";
+	cout << hname << "Histo does not exist\n";
 	bad_det[count_bad] = DetNum;
 	bad_front[count_bad] = FrontChNum;
 	bad_back[count_bad] = BackChNum;
@@ -152,7 +157,6 @@ void SiRelativeGains_Step1(void)
     }
   }
   outfile.close();
-  
   cout << "List of bad detectors:\n";
   for (int i=0; i<count_bad; i++){
     cout << bad_det[i] << "  " << bad_front[i] << "  " << bad_back[i] << endl;
