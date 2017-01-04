b6a8692b897ade7d21f24be1583f28f471678833
Jon Lighthall <jonlighthall@users.noreply.github.com>
2017-01-04 20:08:11 +0000

    Summary: Legends added, toolbar added to Method 2, Method 2 repaired (added vectors), Method 4 added! Automatic gate generation based on TProfile fit.

--- SiCal/SiRelativeCal/QQQ3s/SiRelativeGains_Step1.C
+++ SiCal/SiRelativeCal/QQQ3s/SiRelativeGains_Step1.C
@@ -22,6 +22,7 @@
 #include <TVector.h>
 #include <TROOT.h>
 #include <TProfile.h>
+#include <TLegend.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Double_t MyFit1(TH2F* hist, TCanvas *can) {
@@ -31,6 +32,7 @@ Double_t MyFit1(TH2F* hist, TCanvas *can) {
   Double_t x1[12] = {1450, 630, 3150, 6340, 9200, 10540, 13200, 13600, 11670, 7550, 2400, 1450};
   Double_t y1[12] = {800, 2250, 4900, 8050, 10380, 11520, 13800, 12600, 8700, 5400, 1100, 800};
   TCutG *cut = new TCutG("cut",12,x1,y1);
+  cut->SetLineColor(6);
   cut->Draw("same");
   
   Int_t counter = 0;
@@ -64,7 +66,16 @@ Double_t MyFit1(TH2F* hist, TCanvas *can) {
   graph->Draw("*same");
 
   TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
+  //fun2->SetLineWidth(1);
+  //fun2->SetLineColor(2);
   graph->Fit("fun2","qROB");
+
+  TLegend *leg = new TLegend(0.1,0.75,0.2,0.9);
+  leg->AddEntry(cut,"cut","l");
+  leg->AddEntry(graph,"graph","p");
+  leg->AddEntry(fun2,"TGraph fit","l");
+  leg->Draw();
+  
   can->Update();
   //can->WaitPrimitive();
 
@@ -83,7 +94,12 @@ Double_t MyFit2(TH2F* hist, TCanvas *can) {
   // draw their own graphical cut. To do this, in the canvas: View->Toolbar and click on the scissors on the top
   // right hand corner. Then, select the region around your data by clicking. Double click to close the cut.
   // A best fit line should appear through your data
+  if(!(can->GetShowEventStatus()))can->ToggleEventStatus();
+  if(!(can->GetShowToolBar()))can->ToggleToolBar();
   hist->Draw("colz");
+
+  vector<double> x1;
+  vector<double> y1;
   
   TCutG *cut;
   cut = (TCutG*)can->WaitPrimitive("CUTG");
@@ -94,7 +110,7 @@ Double_t MyFit2(TH2F* hist, TCanvas *can) {
     cut->GetPoint(n,x1[n],y1[n]);
     cout << x1[n] << "\t" << y1[n] << endl;
   }
-  
+  cut->SetLineColor(6);
   cut->Draw("same");
   
   Int_t counter = 0;
@@ -157,6 +173,7 @@ Double_t MyFit3(TH2F* hist, TCanvas *can) {
   Double_t x1[12] = {1450, 630, 3150, 6340, 9200, 10540, 13200, 13600, 11670, 7550, 2400, 1450};
   Double_t y1[12] = {800, 2250, 4900, 8050, 10380, 11520, 13800, 12600, 8700, 5400, 1100, 800};
   TCutG *cut = new TCutG("cut",12,x1,y1);
+  cut->SetLineColor(6);
   
   Int_t counter = 0;
   for (int i=1; i<hist->GetNbinsX(); i++) {
@@ -188,8 +205,8 @@ Double_t MyFit3(TH2F* hist, TCanvas *can) {
 
   TGraph *graph = new TGraph(counter,x,y);
   TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
-  fun2->SetLineWidth(1);
-  fun2->SetLineColor(1);
+  //fun2->SetLineWidth(1);
+  //fun2->SetLineColor(2);
   graph->Fit("fun2","qROB");
   
   hist->ProfileX();
@@ -200,9 +217,9 @@ Double_t MyFit3(TH2F* hist, TCanvas *can) {
   xprof->SetMarkerStyle(4);
   xprof->SetMarkerSize(0.5);
   TF1 *fun3 = new TF1("fun3","[0]*x +[1]",0,16384);
-  fun3->SetLineColor(2);
-  fun3->SetLineStyle(1);
-  fun3->SetLineWidth(1);
+  fun3->SetLineColor(4);
+  fun3->SetLineStyle(2);
+  fun3->SetLineWidth(2);
   
   hist->Draw("colz");
   cut->Draw("same");
@@ -211,12 +228,23 @@ Double_t MyFit3(TH2F* hist, TCanvas *can) {
   xprof->Draw("same");
   xprof->Fit("fun3","q");
 
+  TLegend *leg = new TLegend(0.1,0.75,0.2,0.9);
+  leg->AddEntry(cut,"cut","l");
+  leg->AddEntry(graph,"graph","p");
+  leg->AddEntry(fun2,"TGraph fit","l");
+  leg->AddEntry(fun1,"TH2F fit","l");
+  leg->AddEntry(xprof,"x-profile","pe");  
+  leg->AddEntry(fun3,"TProfile fit","l");   
+  leg->Draw();
+  
   can->Update();
-  can->WaitPrimitive();
+  //can->WaitPrimitive();
 
   Double_t gain = fun2->GetParameter(0);
   Double_t gain2 = fun3->GetParameter(0);
-  printf(" gain ratio is %f\n",fabs(gain-gain2)/gain);
+  Double_t ra=fabs(gain-gain2)/gain;
+  if(ra>0.016)
+    printf("  difference in gain (Meth. 1 vs Meth. 3) is %f\n",ra);
   delete x;
   delete y;
   delete graph;
@@ -226,6 +254,107 @@ Double_t MyFit3(TH2F* hist, TCanvas *can) {
   delete fun3;
 
   return gain2;
+}
+
+Double_t MyFit4(TH2F* hist, TCanvas *can) {
+  //Method 1 - automated cut generation based on TProfile slope
+  can->Clear();
+  hist->Draw("colz");
+  Int_t up=6000;
+  hist->GetXaxis()->SetRangeUser(0,up);
+  hist->GetYaxis()->SetRangeUser(0,up);
+  hist->ProfileX();
+  TString hname;
+  hname=hist->GetName();
+  hname+="_pfx";
+  TProfile *xprof=(TProfile *)gROOT->FindObject(hname.Data());
+  xprof->SetMarkerStyle(4);
+  xprof->SetMarkerSize(0.125);
+  xprof->Draw("same");
+  TF1 *fun3 = new TF1("fun3","[0]*x +[1]",0,16384);
+  fun3->SetLineColor(4);
+  fun3->SetLineStyle(2);
+  fun3->SetLineWidth(2);
+  xprof->Fit("fun3","q");
+  Double_t slope = fun3->GetParameter(0);
+  Double_t offset = fun3->GetParameter(1);
+
+  //Set cut shape here; assumes form y=mx+b
+  Double_t x1=500; 
+  Double_t x2=13000;
+  Double_t width=400;
+  //The corners of a parallelepiped cut window are then calculated
+  Double_t y1=slope*x1+offset;
+  Double_t y2=slope*x2+offset;
+  Double_t dx=(width/2.0)*slope/sqrt(1+slope*slope);
+  Double_t dy=(width/2.0)*1/sqrt(1+slope*slope);
+  const Int_t nv = 5;//set number of verticies
+  Double_t xc[nv] = {x1+dx,x2+dx,x2-dx,x1-dx,x1+dx};
+  Double_t yc[nv] = {y1-dy,y2-dy,y2+dy,y1+dy,y1-dy};
+  TCutG *cut = new TCutG("cut",nv,xc,yc);
+  cut->SetLineColor(6);
+  cut->Draw("same");
+  
+  Int_t counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++) {
+    for (int j=1; j<hist->GetNbinsY(); j++) {
+      if ( !cut->IsInside(hist->GetXaxis()->GetBinCenter(i),hist->GetYaxis()->GetBinCenter(j))) {
+	continue;
+      }
+      counter+=(Int_t)hist->GetBinContent(i,j);
+    }
+  }
+
+  printf(" for %s counts = %d in window\n",hist->GetName(),counter);
+  Double_t *x = new Double_t[counter];
+  Double_t *y = new Double_t[counter];
+
+  counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      if ( !cut->IsInside(hist->GetXaxis()->GetBinCenter(i),hist->GetYaxis()->GetBinCenter(j))) {
+	continue;
+      }
+      for (int k=0; k<hist->GetBinContent(i,j); k++){
+  	x[counter] = hist->GetXaxis()->GetBinCenter(i);
+  	y[counter] = hist->GetYaxis()->GetBinCenter(j);
+  	counter++;
+      }
+    }
+  }
+
+  TGraph *graph = new TGraph(counter,x,y);
+  //graph->SetMarkerSize();
+  //graph->Draw("same*");
+  TF1 *fun2 = new TF1("fun2","[0]*x +[1]",x1,x2);
+  graph->Fit("fun2","qROB");
+  
+  fun2->Draw("same");
+  fun3->Draw("same");
+  
+  TLegend *leg = new TLegend(0.1,0.75,0.2,0.9);
+  leg->AddEntry(xprof,"x-profile","pe");  
+  leg->AddEntry(fun3,"TProfile fit","l");   
+  leg->AddEntry(cut,"cut","l");
+  //leg->AddEntry(graph,"graph","p");
+  leg->AddEntry(fun2,"TGraph fit","l");
+  leg->Draw();
+  
+  can->Update();
+  //can->WaitPrimitive();
+
+  Double_t gain = fun2->GetParameter(0);
+  Double_t ra=fabs(gain-slope)/slope;
+  if(ra>0.016)
+    printf("  difference in gain (Meth. 3 vs Meth. 4) is %f\n",ra);
+  delete x;
+  delete y;
+  delete graph;
+  delete xprof;
+  delete fun2;
+  delete fun3;
+
+  return gain;
 }
 
 void SiRelativeGains_Step1(void)
@@ -284,8 +413,8 @@ void SiRelativeGains_Step1(void)
 	continue;
       }
 
-      Double_t gain = MyFit1b(hist,can);
-      slope[DetNum][FrontChNum+16] = slope[DetNum][FrontChNum+16]*gain;
+      Double_t gain = MyFit4(hist,can); //set fit method here
+      slope[DetNum][FrontChNum+16] = vslope[DetNum][FrontChNum+16]*gain;
     }
     for (Int_t i=0; i<32; i++){
       outfile << DetNum << "\t" << i << "\t" << slope[DetNum][i] << endl;
