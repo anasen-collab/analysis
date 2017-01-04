de158607ea11c6b45934f8e78d2573d76a479ab5
Jon Lighthall <jonlighthall@users.noreply.github.com>
2017-01-04 15:34:14 +0000

    Summary: Added original Method 1 and Method 2 to working copy, modified for generic bin width. New developed method is now Method 3 and outputs the profile slope. Commented-out lines removed, gain ratio printed.

--- SiCal/SiRelativeCal/QQQ3s/SiRelativeGains_Step1.C
+++ SiCal/SiRelativeCal/QQQ3s/SiRelativeGains_Step1.C
@@ -5,6 +5,7 @@
 
 
 //// Edited by : John Parker , 2016Jan22
+//   Developed by : Jon Lighthall, 2016.01
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <TMath.h>
 #include <TCanvas.h>
@@ -23,9 +24,130 @@
 #include <TProfile.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Double_t MyFit(TH2F* hist, TCanvas *can) {
+Double_t MyFit1(TH2F* hist, TCanvas *can) {
+  //Method 1 - calculates slope of points wihtin pre-defined cut using TGraph
+  hist->Draw("colz");
+  
+  Double_t x1[12] = {1450, 630, 3150, 6340, 9200, 10540, 13200, 13600, 11670, 7550, 2400, 1450};
+  Double_t y1[12] = {800, 2250, 4900, 8050, 10380, 11520, 13800, 12600, 8700, 5400, 1100, 800};
+  TCutG *cut = new TCutG("cut",12,x1,y1);
+  cut->Draw("same");
+  
+  Int_t counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      if ( !cut->IsInside(hist->GetXaxis()->GetBinCenter(i),hist->GetYaxis()->GetBinCenter(j))) {
+	continue;
+      }
+      counter+=(Int_t)hist->GetBinContent(i,j);
+    }
+  }
+
+  Double_t *x = new Double_t[counter];
+  Double_t *y = new Double_t[counter];
+  
+  counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      if ( !cut->IsInside(hist->GetXaxis()->GetBinCenter(i),hist->GetYaxis()->GetBinCenter(j))) {
+	continue;
+      }
+      for (int k=0; k<hist->GetBinContent(i,j); k++){
+  	x[counter] = hist->GetXaxis()->GetBinCenter(i);
+  	y[counter] = hist->GetYaxis()->GetBinCenter(j);
+  	counter++;
+      }
+    }
+  }
+
+  TGraph *graph = new TGraph(counter,x,y);
+  graph->Draw("*same");
+
+  TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
+  graph->Fit("fun2","qROB");
+  can->Update();
+  //can->WaitPrimitive();
+
+  Double_t gain = fun2->GetParameter(0);
+  delete x;
+  delete y;
+  delete graph;
+  delete fun2;
+  
+  return gain;
+}
+
+Double_t MyFit2(TH2F* hist, TCanvas *can) {
+  //Method 2 - calculates slope of points wihtin user-defined cut using TGraph
+  // This method works very similar to method 1, except that instead of a predefined cut, the user must
+  // draw their own graphical cut. To do this, in the canvas: View->Toolbar and click on the scissors on the top
+  // right hand corner. Then, select the region around your data by clicking. Double click to close the cut.
+  // A best fit line should appear through your data
+  hist->Draw("colz");
+  
+  TCutG *cut;
+  cut = (TCutG*)can->WaitPrimitive("CUTG");
+  x1.resize(cut->GetN());
+  y1.resize(cut->GetN());
+
+  for(int n=0;n<cut->GetN();n++){
+    cut->GetPoint(n,x1[n],y1[n]);
+    cout << x1[n] << "\t" << y1[n] << endl;
+  }
+  
+  cut->Draw("same");
+  
+  Int_t counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      if ( !cut->IsInside(hist->GetXaxis()->GetBinCenter(i),hist->GetYaxis()->GetBinCenter(j))) {
+	continue;
+      }
+      counter+=(Int_t)hist->GetBinContent(i,j);
+    }
+  }
+
+  Double_t *x = new Double_t[counter];
+  Double_t *y = new Double_t[counter];
+  
+  counter = 0;
+  for (int i=1; i<hist->GetNbinsX(); i++){
+    for (int j=1; j<hist->GetNbinsY(); j++){
+      if ( !cut->IsInside(hist->GetXaxis()->GetBinCenter(i),hist->GetYaxis()->GetBinCenter(j))) {
+	continue;
+      }
+      for (int k=0; k<hist->GetBinContent(i,j); k++){
+  	x[counter] = hist->GetXaxis()->GetBinCenter(i);
+  	y[counter] = hist->GetYaxis()->GetBinCenter(j);
+  	counter++;
+      }
+    }
+  }
+
+  TGraph *graph = new TGraph(counter,x,y);
+  graph->Draw("*same");
+
+  TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
+  graph->Fit("fun2","qROB");
+  can->Update();
+  //can->WaitPrimitive();
+
+  Double_t gain = fun2->GetParameter(0);
+  delete x;
+  delete y;
+  delete graph;
+  delete fun2;
+  
+  return gain;
+}
+
+Double_t MyFit3(TH2F* hist, TCanvas *can) {
+  //Expanded Method 1 - three slopes are calculated:
+  // 1) slope from un-gated fit of 2D histogram
+  // 2) slope from gated fit of TGraph (same as Method 1)
+  // 3) slope from un-gated fit of profile of 2D histogram (uses the average y-position for each x-position)
   can->Clear();
-  //hist->Draw("colz");
+
   TF1 *fun1 = new TF1("fun1","[0]*x +[1]",0,16384);
   fun1->SetLineColor(3);
   fun1->SetLineStyle(2);
@@ -35,8 +157,6 @@ Double_t MyFit(TH2F* hist, TCanvas *can) {
   Double_t x1[12] = {1450, 630, 3150, 6340, 9200, 10540, 13200, 13600, 11670, 7550, 2400, 1450};
   Double_t y1[12] = {800, 2250, 4900, 8050, 10380, 11520, 13800, 12600, 8700, 5400, 1100, 800};
   TCutG *cut = new TCutG("cut",12,x1,y1);
-  //cut = (TCutG*)can->WaitPrimitive("CUTG");
-  //cut->Draw("same");
   
   Int_t counter = 0;
   for (int i=1; i<hist->GetNbinsX(); i++) {
@@ -67,14 +187,12 @@ Double_t MyFit(TH2F* hist, TCanvas *can) {
   }
 
   TGraph *graph = new TGraph(counter,x,y);
-  //graph->Draw("*same");
   TF1 *fun2 = new TF1("fun2","[0]*x +[1]",0,16000);
   fun2->SetLineWidth(1);
   fun2->SetLineColor(1);
   graph->Fit("fun2","qROB");
-  //fun2->Draw("same");
   
-    hist->ProfileX();
+  hist->ProfileX();
   TString hname;
   hname=hist->GetName();
   hname+="_pfx";
@@ -97,15 +215,17 @@ Double_t MyFit(TH2F* hist, TCanvas *can) {
   can->WaitPrimitive();
 
   Double_t gain = fun2->GetParameter(0);
-   delete x;
-   delete y;
-   delete graph;
-   delete xprof;
-   delete fun1;
-   delete fun2;
-   delete fun3;
+  Double_t gain2 = fun3->GetParameter(0);
+  printf(" gain ratio is %f\n",fabs(gain-gain2)/gain);
+  delete x;
+  delete y;
+  delete graph;
+  delete xprof;
+  delete fun1;
+  delete fun2;
+  delete fun3;
 
-  return gain;
+  return gain2;
 }
 
 void SiRelativeGains_Step1(void)
@@ -156,7 +276,7 @@ void SiRelativeGains_Step1(void)
       TString hname=Form("Q3_back_vs_front%i_%i_%i",DetNum,FrontChNum,BackChNum);
       hist = (TH2F*)f1->Get(hname.Data());
       if (hist==NULL){
-	cout << hname << "Histo does not exist\n";
+	cout << hname << " histogram does not exist\n";
 	bad_det[count_bad] = DetNum;
 	bad_front[count_bad] = FrontChNum;
 	bad_back[count_bad] = BackChNum;
@@ -164,7 +284,7 @@ void SiRelativeGains_Step1(void)
 	continue;
       }
 
-      Double_t gain = MyFit(hist,can);
+      Double_t gain = MyFit1b(hist,can);
       slope[DetNum][FrontChNum+16] = slope[DetNum][FrontChNum+16]*gain;
     }
     for (Int_t i=0; i<32; i++){
