902dfe242f4e30fc500da12a7a268a3367a57d6e
Jon Lighthall <jlighthall@fsu.edu>
2017-04-07 17:49:30 -0400

    Update Draw to work with Main.cpp
    Remove the commented-out lines which refer to SX3 detectors. Add handling for missing histograms and update histogram names.

--- SiCal/SiRelativeCal/QQQ3s/images/Draw.C
+++ SiCal/SiRelativeCal/QQQ3s/images/Draw.C
@@ -1,13 +1,7 @@
 {
-  TFile *f = new TFile("run235_245out_Step1_012816.root");
+  //TFile *f1 = new TFile("run235_245out_Step1_012816.root");
+  TFile *f1 = new TFile("../../../../root/run1226-9mQ1.root");
   TCanvas *can = new TCanvas("can","can",900,1100);
-
-  /*
-  TH2F *hist = up_vs_down4_back0;
-  TH2F *hist2 = up_vs_down4_back1;
-  TH2F *hist3 = up_vs_down4_back2;
-  TH2F *hist4 = up_vs_down4_back3;
-  */
 
   Double_t x1[5] = { 1550, 8071, 7065, 1000, 1550 };
   Double_t y1[5] = { 1100, 7150, 8500, 2400, 1100 };
@@ -16,7 +10,12 @@
   can->Divide(2,2);
   Int_t counter = 1;
   for (Int_t DetNum=0; DetNum<4; DetNum++){
-    TH2F *hist = (TH2F*)f->Get(Form("QQQback_vs_front%i",DetNum));
+    TString hname=Form("Q3_back_vs_front%i",DetNum);
+    hist = (TH2F*)f1->Get(hname.Data());
+    if (hist==NULL) {
+      cout << hname << " histogram does not exist\n";
+	continue;
+    }
     TF1 *fun = new TF1("fun","x",0,10000);
     can->cd(counter);
     hist->Draw("colz");
