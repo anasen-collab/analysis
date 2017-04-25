f0c439eb48ef0778ee6a2a9b64746b59544565fe
Jon Lighthall <jlighthall@fsu.edu>
2017-04-25 13:24:25 -0400

    working

--- SiCal/EnergyCal/SiAlpha_All.C
+++ SiCal/EnergyCal/SiAlpha_All.C
@@ -29,80 +29,73 @@ void SiAlpha_All(void) {
   //Float_t Energies[npeaks] = {5.42315,5.68537,6.28808,6.77803,8.74886};
   Float_t Energies[npeaks] = {5,7,10};
 
-  TCanvas *c1 = new TCanvas();
-  c1->Divide(1,2);
 
-  //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/253Si_Alpha_Cal.root");
+   //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/253Si_Alpha_Cal.root");
   //TFile *f1 = new TFile("/data0/nabin/ANASEN/ANASEN_NKJ/ANASEN_N/Calibration/253-60_Si_Alpha_Cal.root");
-  TFile *f1 = new TFile("/home/lighthall/anasen/root/run1255-61.root");
+  //TFile *f1 = new TFile("/home/lighthall/anasen/root/run1255-61.root");
+  TFile *f1 = new TFile("/home/lighthall/anasen/root/run1255-61mQ2S3_geo_init.root");
+  
   if ( !f1->IsOpen() ) {
     cout << "Error: Root file does not exist\n";
     exit(EXIT_FAILURE);
   }
-  
-  TH1I *h1 = new TH1I("h1","h1",3000,0,range);
-  TTree *DataTree = (TTree*)f1->Get("DataTree");
-  TF1 *fit = new TF1("fit","pol1",0,range);
+    
+  TTree *MainTree = (TTree*)f1->Get("MainTree");
+  if (MainTree==NULL){
+    cout << "Tree does not exist\n";
+    exit(EXIT_FAILURE);
+  }
+  ofstream outfile;
+  outfile.open("saves/SiAlpha.dat");
+  outfile << "DetNum\tOffset\tSlope\n";
 
+  TCanvas *can = new TCanvas();
+  can->Divide(1,2);
+  TH1I *hist = new TH1I("hist","hist",3000,0,range);
+  TF1 *fit = new TF1("fit","pol1",0,range);
   Double_t zeroshift = 0;
   Double_t MeVperCh = 0;
   Double_t q0 = 0;
   TSpectrum *s = 0;
   TGraph *FitGraph = 0;
-  
-  ofstream outfile;
-  outfile.open("saves/SiAlpha.dat");
-  
-  for (Int_t MBID=1; MBID<3; MBID++) {  
-    for (Int_t CBID=1; CBID<15; CBID++) { 
-      ////for the front of the detectors //run250 || 267
-      //if((CBID==1 || CBID==2 || CBID==5 || CBID==6 || CBID==9 ||CBID==10) ||(MBID==1 && (CBID==11 || CBID==12 || CBID==13 || CBID==14))) {
 
-      ////for the back of the detectors //run251 || 268
-      //if((CBID==3 || CBID==4 || CBID==7 || CBID==8) || (MBID==2 && (CBID==11 || CBID==12))) {	 
-      //if(MBID==2 && (CBID==3 || CBID==4 || CBID==7 || CBID==8 || CBID==11 || CBID==12)) {	 
-	
-	for (Int_t ChNum=0; ChNum<16; ChNum++) {
-	  //// Mask bad channels  //that can create problem in cruising calibration.
-
-	  if (MBID==1 && CBID==1 && (ChNum==4 ||ChNum ==14)) { //bad at front 
+  for (Int_t DetNum=0; DetNum<28; DetNum++) {
+    // if (DetNum==0 || DetNum==3) {
+    //   Energies[0] = 4;
+    //   Energies[1] = 6;
+    //   Energies[2] = 9;
+    // }
+     	  can->cd(1);	    
+	  MainTree->Draw("EnergyBack>>hist",Form("DetID==%i && (HitType==111 || HitType==11)",DetNum),"");
+	  hist->SetTitle(Form("Det%i",DetNum));  
+	  
+	  //can->WaitPrimitive();
+	  if(hist->GetEntries()==0) {
+	    printf("Histogram %s has zero entries.\n",hist->GetTitle());
 	    continue;
-	  }	      
-	  if((MBID==1 && CBID==8 && (ChNum==0 || ChNum==1 || ChNum==2))||(MBID==2 && CBID ==8 && (ChNum==1 || ChNum==2))) {//bad at back 
-	    continue;
-	  } 
-	  if(MBID == 2 && CBID == 7 && (ChNum ==4 || ChNum ==10)) {//bad at back 
-	    continue;
-	  }
-		
-	  c1->cd(1);	    
-	  DataTree->Draw("Si.Energy>>h1",Form("Si.MBID==%d && Si.CBID==%d && Si.ChNum==%d",MBID,CBID,ChNum));
-	 
-	  //c1->WaitPrimitive();
-	  if(h1->GetEntries()==0) continue;
+ 	  }
 	      
 	  if(s!=0) {
 	    delete s;
 	  }
 
 	  TSpectrum *s = new TSpectrum();
-	       
-	  h1->SetTitle(Form("MBID %d CBID %d ChNum %d",MBID,CBID,ChNum));  
-	  h1->GetXaxis()->SetRangeUser(1500,6000);
-	  c1->Update();
 	  
-	  //Int_t nfound = s->Search(h1,15,"",0.25);
-	  //Int_t nfound = s->Search(h1,5," nobackground",0.10);
-	  Int_t nfound = s->Search(h1,10," ",0.05);
-
+	  hist->GetXaxis()->SetRangeUser(1500,6000);
+	  can->Update();
+	  
+	  //Int_t nfound = s->Search(hist,15,"",0.25);
+	  //Int_t nfound = s->Search(hist,5," nobackground",0.10);
+	  Int_t nfound = s->Search(hist,10," ",0.05);
 	  Float_t *xpeaks = s->GetPositionX();
-	  Float_t Temp=0;
 
 	  if(nfound <npeaks) {
 	    printf("Less than %d peaks found. Aborting.\n",npeaks);
 	    continue;
 	  }
-	      
+
+	  //sort peaks in order of channle number
+	  Float_t Temp=0;    
 	  for(Int_t i=0;i<nfound;i++) {
 	    for(Int_t j=i;j<nfound;j++) {
 	      if (xpeaks[j] < xpeaks[i]) {
@@ -112,29 +105,25 @@ void SiAlpha_All(void) {
 	      }
 	    }
 	  }
-	    
+	  
 	  if(FitGraph!=0) {
 	    delete FitGraph;
 	  }
 
 	  TGraph *FitGraph = new TGraph(nfound,xpeaks, &(Energies[0]));
-	  c1->cd(2);
+	  can->cd(2);
 	      
 	  FitGraph->Draw("AP*");
-	  FitGraph->Fit("fit","qROB=0.95");
+	  FitGraph->Fit("fit","qROB=1");
 	  ////FitGraph->Fit("fit","E");
 	  zeroshift = fit->GetParameter(0);
 	  MeVperCh = fit->GetParameter(1);
 	  q0 = -zeroshift/MeVperCh;
 	  // cout << zeroshift << " " << MeVperCh << endl;;
-	  cout << MBID << " " << CBID << " " << ChNum << " q0 = "<<q0<<endl;
-	  c1->Update();
-	  outfile << MBID << "\t" << CBID << "\t" << ChNum << "\t"<< zeroshift << "\t" << MeVperCh <<endl;
-	  //outfile << MBID << "\t" << zeroshift << "\t" << MeVperCh <<endl;
-	  //outfile << MBID << "\t" << CBID << "\t" << ChNum << "\t"<< zeroshift << "\t" << MeVperCh << "\t"<< q0 <<endl;
-	}
-	//}
-    }
+	  cout << DetNum << " q0 = "<<q0<<endl;
+	  can->Update();
+	  outfile << DetNum << "\t"<< zeroshift << "\t" << MeVperCh <<endl;
+    
   }
   delete FitGraph;
   delete s;
