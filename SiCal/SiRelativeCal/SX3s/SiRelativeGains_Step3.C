7a7032abb6b9d05b14aab9bb2eedddef9392372c
Jon Lighthall <jlighthall@fsu.edu>
2017-04-26 17:58:11 -0400

    add offsets to Step3

--- SiCal/SiRelativeCal/SX3s/SiRelativeGains_Step3.C
+++ SiCal/SiRelativeCal/SX3s/SiRelativeGains_Step3.C
@@ -7,6 +7,7 @@
 // Developed by : Jon Lighthall, 2017.04
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //C++
+#include <fstream>
 #include <exception>
 //ROOT
 #include <TMath.h>
@@ -15,15 +16,12 @@
 #include <TTree.h>
 #include <TH1F.h>
 #include <TH2F.h>
-#include <TF1.h>
 //Methods
 #include "SiRelativeGains.h"
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void SiRelativeGains_Step3(void)
-{
+void SiRelativeGains_Step3(void) {
   using namespace std;
-
-  TFile *f1 = new TFile("/home/lighthall/anasen/root/run1226-9mQ2S3.root");
+  TFile *f1 = new TFile("/home/lighthall/anasen/root/run1226-9mQ2_fix.root");
   if ( !f1->IsOpen() ){
     cout << "Error: Root file does not exist\n";
     exit(EXIT_FAILURE);
@@ -31,16 +29,19 @@ void SiRelativeGains_Step3(void)
   
   //Input the .dat file used by Main.cpp to generate the .root file given above
   Gains gains;
-  gains.Load("saves/X3RelativeGains_Step3_170418.dat");
+  gains.Load("saves/X3RelativeGains_Step2.dat");
   gains.Save("saves/X3RelativeGains_Step3");
+  Offsets offsets;
+  offsets.Load("saves/X3FinalFix_Step2.dat");
+  offsets.Save("saves/X3FinalFix_Step3");
   
   TCanvas *can = new TCanvas("can","can",800,600);
 
   BadDetectors bad;
   GainMatch gainmatch;
 
-  for (Int_t DetNum=4; DetNum<28; DetNum++) {
-    for (Int_t BackChNum=0; BackChNum<4; BackChNum++){
+  for (Int_t DetNum=4; DetNum<ndets; DetNum++) {
+    for (Int_t BackChNum=0; BackChNum<4; BackChNum++) {
       TH2F *hist = NULL;
       TString hname=Form("back_vs_front%i_b%i",DetNum,BackChNum);
       hist = (TH2F*)f1->Get(hname.Data());
@@ -48,14 +49,17 @@ void SiRelativeGains_Step3(void)
 	cout << "****" << hname << " histogram does not exist\n";
 	bad.Add(DetNum,-1,BackChNum);
 	gains.Add(DetNum-4,BackChNum,0,0);
+	offsets.Add(DetNum-4,BackChNum,0,0);
 	continue;
       }
-
+      
       Double_t gain = gainmatch.Fit4(hist,can,1);
       gains.Add(DetNum-4,BackChNum,gain,1.0/gain);
+      offsets.Add(DetNum-4,BackChNum,offset,-offset);
     }
     for (Int_t i=0; i<12; i++){
-      outfile << DetNum << "\t" << i << "\t" << gains.old[DetNum-4][i] << endl;
+      outfile        << DetNum << "\t" << i << "\t" <<   gains.old[DetNum-4][i] << endl;
+      outfile_offset << DetNum << "\t" << i << "\t" << offsets.old[DetNum-4][i] << endl;
     }
   }
   bad.Print();
