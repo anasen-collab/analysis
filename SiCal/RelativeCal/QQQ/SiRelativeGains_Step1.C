a41590629de0c676c41061eb05f6916b2d44036e
Jon Lighthall <jlighthall@fsu.edu>
2017-05-02 16:48:32 -0400

    Update Q3 rel cal to use TGraph from TTree
    Update from Fri Apr 28: Move file declaration to header file; add TTree to TGraph fits: Fit7, Fit8. Add beeps to BadDet::Print(), remove unneeded gain variable from Fit1, Fit2, Fit4; update file names, update histogram names for Q3 Step 2! Format bad histogram print.

--- SiCal/RelativeCal/QQQ/SiRelativeGains_Step1.C
+++ SiCal/RelativeCal/QQQ/SiRelativeGains_Step1.C
@@ -21,8 +21,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void SiRelativeGains_Step1(void) {
   using namespace std;
-  TFile *f1 = new TFile("/home/lighthall/anasen/root/run1227mQ2S3.root");
-  //TFile *f1 = new TFile("/home/lighthall/anasen/root/run1226-9mQ2S2_fix.root");
+  f1 = new TFile("/home/lighthall/anasen/root/run1227mQ2S3.root");
+  //f1 = new TFile("/home/lighthall/anasen/root/run1226-9mQ2_fix.root");
   if ( !f1->IsOpen() ){
     cout << "Error: Root file does not exist\n";
     exit(EXIT_FAILURE);
@@ -31,10 +31,10 @@ void SiRelativeGains_Step1(void) {
   //Input the .dat file used by Main.cpp to generate the .root file given above
   Gains gains;
   //gains.Load("saves/QQQRelativeGains_Slope1.dat");
-  gains.Load("saves/QQQRelativeGains_Step2_170427.dat");
+  gains.Load("saves/QQQRelativeGains_Step2_170428.dat");
   gains.Save("saves/QQQRelativeGains_Step1");
   Offsets offsets;
-  offsets.Load("saves/QQQFinalFix_Step2_170427.dat");
+  offsets.Load("saves/QQQFinalFix_Step2_170428.dat");
   offsets.Save("saves/QQQFinalFix_Step1");
     
   TCanvas *can = new TCanvas("can","can",1362,656);
@@ -59,13 +59,14 @@ void SiRelativeGains_Step1(void) {
       TString hname=Form("Q3_back_vs_front%i_%i_%i",DetNum,FrontChNum,BackChNum);
       hist = (TH2F*)f1->Get(hname.Data());
       if (hist==NULL) {
-	cout << hname << " histogram does not exist\n";
+	cout << "****" << hname << " histogram does not exist\n";
 	bad.Add(DetNum,FrontChNum,BackChNum);
 	gains.Add(DetNum,FrontChNum+16,0,0);
 	offsets.Add(DetNum,FrontChNum+16,0,0);
 	continue;
       }
-      Double_t gain = gainmatch.Fit6(hist,can); //set fit method here
+      //Double_t gain = gainmatch.Fit6(hist,can); //set fit method here
+      Double_t gain = gainmatch.Fit7(DetNum,FrontChNum,BackChNum);
       gains.Add(DetNum,FrontChNum+16,gain,gain);
       offsets.Add(DetNum,FrontChNum+16,offset,offset);
       //}//back loop
