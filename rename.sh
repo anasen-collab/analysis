d1f076c7cfd64d325e3f69ea4f1bcc626811ce53
Jon Lighthall <jlighthall@fsu.edu>
2017-05-10 15:43:00 -0400

    update rename to ignore missing directories

--- rename.sh
+++ rename.sh
@@ -1,9 +1,9 @@
-REWRITE_FROM='PCCal/PosCal'
-REWRITE_TO='PCCal/PositionCal'
+REWRITE_FROM='SiCal/SiRelativeCal'
+REWRITE_TO='SiCal/RelativeCal'
 echo $REWRITE_FROM
 echo $REWRITE_TO
 
 git filter-branch -f --index-filter "
         git read-tree --prefix='$REWRITE_TO'/ \$GIT_COMMIT:'$REWRITE_FROM'
-        git rm -r --cached '$REWRITE_FROM'
+        git rm -r --cached --ignore-unmatch '$REWRITE_FROM'
 "
