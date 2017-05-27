002c079c2eec6d70bcf927ae67f1781f59d0321b
Jon Lighthall <jlighthall@fsu.edu>
2017-05-27 12:04:19 -0400

    add PC calfile

--- PCCal/PositionCal/PosCal.C
+++ PCCal/PositionCal/PosCal.C
@@ -17,78 +17,11 @@
 #include <TF1.h>
 #include <TSpectrum.h>
 #include <TCutG.h>
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Double_t MyFit(TH2F* hist, TCanvas* can) {
-  hist->Draw("colz");
-
-  vector<double> x1;
-  vector<double> y1;
-
-  TCutG *cut;
-  cut = (TCutG*)can->WaitPrimitive("CUTG");
-  x1.resize(cut->GetN());
-  y1.resize(cut->GetN());
-  
-  Int_t counter = 0;
-  for (int i=1; i<hist->GetNbinsX(); i++) {
-    for (int j=1; j<hist->GetNbinsY(); j++) {
-      if ( !cut->IsInside((Double_t)i*0.1667,(Double_t)j*0.1667) ) {
-	continue;
-      }
-      for (int k=0; k<hist->GetBinContent(i,j); k++) {
-	counter++;
-      }
-    }
-  }
-  
-  Double_t *x = new Double_t[counter];
-  Double_t *y = new Double_t[counter];
-
-  counter = 0;
-  for (int i=1; i<hist->GetNbinsX(); i++) {
-    for (int j=1; j<hist->GetNbinsY(); j++) {
-      if ( !cut->IsInside((Double_t)i*0.1667,(Double_t)j*0.1667) ) {
-	continue;
-      }
-      for (int k=0; k<hist->GetBinContent(i,j); k++) {
-	x[counter] = (Double_t)i*0.1667;
-	y[counter] = (Double_t)j*0.1667;
-	counter++;
-      }
-    }
-  }
-  
-  TGraph *graph = new TGraph(counter,x,y);
-  graph->Draw("*same");
-
-  TF1 *fun2 = new TF1("fun2","[0]+[1]*x",0,6000);
-  fun2->SetParameter(0,5000);
-  fun2->SetParameter(1,-1);
-  graph->Fit("fun2");
-  can->Update();
-
-  //cout << fun2->GetChisquare() << "  " << fun2->GetChisquare()/counter;
-
-  can->WaitPrimitive();
-
-  Double_t gain = fun2->GetParameter(1);
-
-  delete x;
-  delete y;
-  delete graph;
-  delete fun2;
-
-  if (gain < -3 || gain > -0.1 ){//basically if the slope is very far from one, I don't trust the fit
-    gain = -1;
-  }
-  return gain;
-}
-
 void PosCal(void) {
   using namespace std;
-  Double_t x[10];
-  Double_t y[10];
+  Double_t x[99];
+  Double_t y[99];
   string rootfile = "/home/lighthall/anasen/root/track/spacer0t.root";
   TFile *f1 = new TFile(rootfile.c_str());
   if (!f1->IsOpen()){
@@ -122,19 +55,24 @@ void PosCal(void) {
   infile.close();
 
   ofstream outfile;
-  outfile.open("saves/PCWireCal_170527_2.dat");
+  outfile.open("saves/PCWireCal_170527_4.dat");
   outfile << "Wire\tSlope\tShift\n";
 
   TCutG *cut;
   for (Int_t DetNum=0; DetNum<24; DetNum++) {
     TH2F *hist = NULL;
+    if(DetNum!=18) continue;
     hist = (TH2F*)f1->Get(Form("PCZ_vs_Z_Clean%i",DetNum));
     if (hist==NULL){
       outfile << DetNum << "\t" << 1 << "\t" << 0 << endl;
       cout << "Warning: hist with wire number " << DetNum << " does not exist.\n";
 	continue;
     }
+    can->SetLogz();
     hist->Draw("colz");
+    hist->GetXaxis()->SetRangeUser(-1.1,1.1);
+    hist->GetYaxis()->SetRangeUser(5,27);
+    
     cut = (TCutG*)can->WaitPrimitive("CUTG");
 	
     for(int n=0;n<cut->GetN()-2;n++){
