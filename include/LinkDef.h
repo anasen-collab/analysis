f7f14be8e5aa9b564890c54a062557ab281b4e95
Jon Lighthall <jlighthall@fsu.edu>
2017-08-15 16:32:18 -0400

    Remove auto-generated and executable files from Organize, ParkerReconstruct; remove duplicate LinkDef; update LinkDef for relative path

--- include/LinkDef.h
+++ include/LinkDef.h
@@ -6,6 +6,7 @@
 #pragma link C++ nestedclasses;
  
 //#pragma link C++ defined_in "/home2/parker/ANASEN/LSU/Include/organizetree.h";
-#pragma link C++ defined_in "/home/manasta/Desktop/parker_codes/Include/organizetree.h";
+//#pragma link C++ defined_in "/home/manasta/Desktop/parker_codes/Include/organizetree.h";
+#pragma link C++ defined_in "organizetree.h";
 
 #endif
